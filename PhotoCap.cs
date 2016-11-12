ec9b65322199da10bc83cc364ec59c02c3db3b3e
atielking <atielking@micds.org>
2016-11-12 12:11:44 -0800

    photo capture script

---
+++ PhotoCap.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.VR.WSA.WebCam;
+
+public class PhotoCap : MonoBehaviour {
+    PhotoCapture photoCaptureObject = null;
+	// Use this for initialization
+	void Start () {
+        PhotoCapture.CreateAsync(false, OnPhotoCaptureCreated); //hologram show, callback
+	}
+
+    void OnPhotoCaptureCreated(PhotoCapture captureObject)
+    {
+        photoCaptureObject = captureObject;
+
+        List<Resolution> resolutions = new List<Resolution>(PhotoCapture.SupportedResolutions);
+        Resolution cameraResolution = resolutions[0];
+
+        CameraParameters c = new CameraParameters();
+        c.hologramOpacity = 0.0f; //can't see it
+        c.cameraResolutionWidth = cameraResolution.width;
+        c.cameraResolutionHeight = cameraResolution.height;
+        c.pixelFormat = CapturePixelFormat.BGRA32;
+
+        captureObject.StartPhotoModeAsync(c, false, OnPhotoModeStarted);
+
+    }
+
+    void OnStoppedPhotoMode(PhotoCapture.PhotoCaptureResult result)
+    {
+        photoCaptureObject.Dispose();
+        photoCaptureObject = null;
+    }
+
+    private void OnPhotoModeStarted(PhotoCapture.PhotoCaptureResult result)
+    {
+        if(result.success)
+        {
+            string filename = string.Format(@"CapturedImage{0}_n.jpg", Time.time);
+            Debug.Log(Application.persistentDataPath);
+            string filePath = System.IO.Path.Combine(Application.persistentDataPath, filename);
+
+            photoCaptureObject.TakePhotoAsync(filePath, PhotoCaptureFileOutputFormat.JPG, OnCapturedPhotoToDisk);
+        }
+        else
+        {
+            Debug.LogError("Unable to start photo mode!");
+        }
+    }
+	
+    void OnCapturedPhotoToDisk(PhotoCapture.PhotoCaptureResult result)
+    {
+        if (result.success)
+        {
+            Debug.Log("Saved Photo to disk!");
+            photoCaptureObject.StopPhotoModeAsync(OnStoppedPhotoMode);
+        }
+        else
+        {
+            Debug.Log("Failed to save Photo to disk");
+        }
+    }
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
